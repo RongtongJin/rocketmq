4ac1df83b1c2d098652f2398c4de33e91e509e35
RongtongJin <jinrongtong16@mails.ucas.ac.cn>
2023-05-05 10:58:24 +0800

    for test

--- store/src/main/java/org/apache/rocketmq/store/ConsumeQueue.java
+++ store/src/main/java/org/apache/rocketmq/store/ConsumeQueue.java
@@ -869,7 +869,7 @@ public class ConsumeQueue implements ConsumeQueueInterface, FileQueueLifeCycle {
         final long cqOffset) {
 
         if (offset + size <= this.maxPhysicOffset) {
-            log.warn("Maybe try to build consume queue repeatedly maxPhysicOffset={} phyOffset={}", maxPhysicOffset, offset);
+            //log.warn("Maybe try to build consume queue repeatedly maxPhysicOffset={} phyOffset={}", maxPhysicOffset, offset);
             return true;
         }
 
